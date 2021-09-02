20a2b7cc2c5d5a80ba58775a006b817fcd479ed4
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-02 11:05:04 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <div class="menu">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Mais Fibras</a>
+              <a class="navbar-brand" href="#">Blog-livewire</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
