29ac78be223d7ee7b1e6929e3a68a36729774778
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-02 19:24:51 +0100

    trabalhando no logout

--- resources/views/livewire/logout.blade.php
+++ resources/views/livewire/logout.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <a class="nav-link" tabindex="-1" aria-disabled="true" href="#" wire:click="logout">Logout</a>
+    <a class="nav-link" tabindex="-1" aria-disabled="true" wire:click="logout">Logout</a>
 </div>
