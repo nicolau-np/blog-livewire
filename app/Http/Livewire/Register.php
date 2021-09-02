f755f0a9d18f26eb0f24bc5d3fb4cdc40a362644
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-02 11:44:58 +0100

    criando form do register

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Register extends Component
 {
+
+    public $email;
+    public $name;
+    public $password;
+    public $confirmpassword;
+
     public function render()
     {
         return view('livewire.register');
--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,3 +1,45 @@
-<div>
-    register
+<div class="form-register">
+    <form wire:submit.prevent="submit">
+        <div class="row">
+            <div class="col-md-12">
+                @if (session()->has('message'))
+                <div class="alert alert-success">{{session('message')}}</div>
+                @endif
+            </div>
+
+            <div class="col-md-12">
+                <input type="text" name="name" wire:model="name" class="form-control" placeholder="Name" />
+                @error('name')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <br/><br/>
+            <div class="col-md-12">
+                <input type="email" name="email" wire:model="email" class="form-control" placeholder="Email"/>
+                @error('email')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <br/><br/>
+            <div class="col-md-12">
+                <input type="password" name="password" wire:model="password" class="form-control" placeholder="Password"/>
+                @error('password')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <br/><br/>
+            <div class="col-md-12">
+                <input type="confirmpassword" name="confirmpassword" wire:model="confirmpassword" class="form-control" placeholder="Confirm Password"/>
+                @error('confirmpassword')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <br/><br/>
+            <div class="col-md-12">
+                <button class="btn btn-primary">
+                        Salvar
+                </button>
+            </div>
+        </div>
+    </form>
 </div>
