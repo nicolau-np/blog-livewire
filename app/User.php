7f19fe938cf9cf71c6755276815733402a1c3bf0
Nicolau NP <mr1Normaliii@gmail.com>
2021-09-02 19:14:25 +0100

    update

--- app/User.php
+++ app/User.php
@@ -19,6 +19,24 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function comment(){
         return $this->hasMany(Comment::class, 'id_user', 'id');
     }
